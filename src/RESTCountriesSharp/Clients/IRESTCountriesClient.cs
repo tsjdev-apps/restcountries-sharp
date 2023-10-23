2a37814eb09ecec09d8a97fc158c9cb4f1aecb47
Sebastian Jensen <apps@tsjdev-apps.de>
2023-10-23 21:16:59 +0200

    Add IRESTCountriesClient by using REFIT for communication with API

---
+++ src/RESTCountriesSharp/Clients/IRESTCountriesClient.cs
@@ -0,0 +1,40 @@
+﻿using Refit;
+using RESTCountriesSharp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RESTCountriesSharp.Clients
+{
+    internal interface IRESTCountriesClient
+    {
+        [Get("/all")]
+        Task<IEnumerable<Country>> GetAllCountriesAsync();
+
+        [Get("/name/{name}")]
+        Task<IEnumerable<Country>> GetCountriesByNameAsync(string name);
+
+        [Get("/name/{name}?fullText=true")]
+        Task<IEnumerable<Country>> GetCountriesByFullNameAsync(string name);
+
+        [Get("/alpha/{code}")]
+        Task<IEnumerable<Country>> GetCountriesByCodeAsync(string code);
+
+        [Get("/alpha")]
+        Task<IEnumerable<Country>> GetCountriesByCodesAsync(string codes);
+
+        [Get("/currency/{currency}")]
+        Task<IEnumerable<Country>> GetCountriesByCurrencyAsync(string currency);
+
+        [Get("/lang/{language}")]
+        Task<IEnumerable<Country>> GetCountriesByLanguageAsync(string language);
+
+        [Get("/capital/{capital}")]
+        Task<IEnumerable<Country>> GetCountriesByCapitalAsync(string capital);
+
+        [Get("/region/{region}")]
+        Task<IEnumerable<Country>> GetCountriesByRegionAsync(string region);
+
+        [Get("/subregion/{subregion}")]
+        Task<IEnumerable<Country>> GetCountriesBySubregionAsync(string subregion);
+    }
+}
